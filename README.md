1bbcf495f6f1adb9b4d9078c70c0bb9d0337fa32
PanaEk Warawit <panaek@feedback180.com>
2021-04-10 05:01:21 +0700

    update parameters

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ This python function split audio file using ffmpeg
 - Windows 10 
 
 ## input parameters
-- ffmpeg_location
 - input_filename
 - output_filename
 - start_time
 - end_time
+- ffmpeg_location
+
+Default ffmpeg_location is embeded into the function.
 
--- ffmpeg_cut_audio.py
+++ ffmpeg_cut_audio.py
@@ -2,15 +2,31 @@ import sys
 import os
 
 def ffmpeg_cut_audio(input_filename, output_filename, start_time, end_time, ffmpeg_location='C:\\"Program Files (x86)"\\"FFmpeg for Audacity"\\'):
-    print(input_filename)
-    print(output_filename)
-    print(start_time)
-    print(end_time)
-    print(ffmpeg_location)
-    cmd_str = ffmpeg_location + 'ffmpeg'
+    '''
+
+        ffmpeg -i input.mp4 -ss 00:00:00 -to 00:10:00 -c copy output1.mp4
+        ffmpeg -i input.mp4 -ss 00:10:00 -to 00:20:00 -c copy output2.mp4
+
+        /**
+        * -i  input file
+        * -ss start time in seconds or in hh:mm:ss
+        * -to end time in seconds or in hh:mm:ss
+        * -c codec to use
+        */
+    '''
+    # print(input_filename)
+    # print(output_filename)
+    # print(start_time)
+    # print(end_time)
+    # print(ffmpeg_location)
+    cmd_str = ffmpeg_location + 'ffmpeg -i ' + input_filename + ' -ss ' + start_time + ' -to ' + end_time + ' ' + output_filename
     print(cmd_str)
     os.system(cmd_str)
 
 
 if __name__ == '__main__':
-    ffmpeg_cut_audio(1,2,3,4)
+    input_filename = '"C:\\Users\\pwara\\OneDrive - infomobius.com\\02-Customer Projects\\NBTC_Voice\\input\\test1.mp4"'
+    output_filename = '"C:\\Users\\pwara\\OneDrive - infomobius.com\\02-Customer Projects\\NBTC_Voice\\input\\test1_output.mp4"'
+    start_time = '00:00:05'
+    end_time = '00:00:10'
+    ffmpeg_cut_audio(input_filename, output_filename, start_time, end_time)
